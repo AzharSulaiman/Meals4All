89e95741c4a37559d7a217b749fadcd20e78b725
Azhar Sulaiman <mdazharsulaiman@gmail.com>
2023-03-26 16:44:29 -0400

    bottom Nav position swithc

--- src/containers/bottom-navigate/index.js
+++ src/containers/bottom-navigate/index.js
@@ -29,14 +29,15 @@ class BottomNavigation extends React.Component {
           <img src={this.props.currentNav == 'requests' ? "assets/requests-active.png" : "assets/requests.png"} alt="request-icon"/>
               <div className={(this.props.currentNav == 'requests' || this.props.currentNav == '/') ? "iconLabel-active" : "iconLabel"}>Requests</div>
           </Link>
-          <Link onClick={()=>this.props.currentNavigation('account')} className="col linkDec" to="/account">
-          <img src={this.props.currentNav == 'account' ? "assets/account-active.png" : "assets/account.png"} alt="account-icon"/>
-              <div className={(this.props.currentNav == 'account' || this.props.currentNav == '/') ? "iconLabel-active" : "iconLabel"}>Account</div>
-          </Link>
           <Link onClick={()=>this.props.currentNavigation('donate')} className="col linkDec" to="/donate">
           <img src={this.props.currentNav == 'donate' ? "assets/donate-active.png" : "assets/donate.png"} alt="donate-icon"/>
               <div className={(this.props.currentNav == 'donate' || this.props.currentNav == '/') ? "iconLabel-active" : "iconLabel"}>Donate</div>
           </Link>
+          <Link onClick={()=>this.props.currentNavigation('account')} className="col linkDec" to="/account">
+          <img src={this.props.currentNav == 'account' ? "assets/account-active.png" : "assets/account.png"} alt="account-icon"/>
+              <div className={(this.props.currentNav == 'account' || this.props.currentNav == '/') ? "iconLabel-active" : "iconLabel"}>Account</div>
+          </Link>
+         
       </div>
     </div> : ''
     }
